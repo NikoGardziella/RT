4d395246750ecca9f685896288987356290a2300
David Malesevic <dmalesev@c1r6p17.hive.fi>
2022-11-03 10:08:10 +0200

    Tried adding fonts and string display (failed). Added fps timer in terminal display to test performance.

--- sources/images/put_images_to_screen.c
+++ sources/images/put_images_to_screen.c
@@ -6,16 +6,36 @@
 /*   By: dmalesev <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 15:05:29 by dmalesev          #+#    #+#             */
-/*   Updated: 2022/11/03 09:17:41 by dmalesev         ###   ########.fr       */
+/*   Updated: 2022/11/03 10:05:53 by dmalesev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rt.h"
 
+double	time_since_success(double ammount, int id)
+{
+	static struct timespec	time[8];
+	struct timespec			curr_time;
+	long int				elapsed;
+
+	clock_gettime(CLOCK_MONOTONIC, &curr_time);
+	elapsed = (curr_time.tv_sec - time[id].tv_sec) * 1000000
+		+ (curr_time.tv_nsec - time[id].tv_nsec) / 1000;
+	if (elapsed > (long int)(ammount * 1000000.0))
+	{
+		time[id] = curr_time;
+	}
+	return ((double)elapsed / 1000000.0);
+}
+
 void	put_images_to_screen(t_env *env)
 {
+	double	plot_time;
+
 	process_image(&env->sdl, &env->img[0], 1, env);
 	process_image(&env->sdl, &env->img[1], 1, env);
 	process_image(&env->sdl, &env->img[2], 1, env);
 	SDL_UpdateWindowSurface(env->sdl.window);
+	plot_time = (double)time_since_success(0.0f, 0);
+	printf("Plot time %f\n", plot_time);
 }
