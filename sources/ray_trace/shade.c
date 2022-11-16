b4e0eb750908f55cff9af4b0b05489e0a968414b
David Malesevic <dmalesev@c1r6p17.hive.fi>
2022-11-16 11:22:31 +0200

    Fixed files having missing files while merging.

--- sources/ray_trace/shade.c
+++ sources/ray_trace/shade.c
@@ -6,7 +6,7 @@
 /*   By: ctrouve <ctrouve@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 15:08:05 by ctrouve           #+#    #+#             */
-/*   Updated: 2022/11/14 09:54:11 by ctrouve          ###   ########.fr       */
+/*   Updated: 2022/11/09 14:59:38 by dmalesev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,67 @@
 
 static double	get_light_level(double t, double lumen, t_3d normal, t_3d dir)
 {
-<<<<<<< HEAD
-	if (a > b)
-		return (a);
+	double	light_intensity;
+	double	difuse;
+	double	light_level;
+
+	light_intensity = t / (lumen * lumen);
+	difuse = dot_product(normal, dir);
+	light_level = difuse - light_intensity;
+	if (light_level < 0.0)
+		light_level = 0;
+	return (light_level);
+}
+
+static t_color	calc_light(t_color final, t_color light, t_color object, double level)
+{
+	t_3i	color_calc;
+
+	color_calc.x = (int)final.channel.r;
+	color_calc.y = (int)final.channel.g;
+	color_calc.z = (int)final.channel.b;
+	color_calc.x += (int)((double)object.channel.r * level * (double)(light.channel.r / 255.0));
+	color_calc.y += (int)((double)object.channel.g * level * (double)(light.channel.g / 255.0));
+	color_calc.z += (int)((double)object.channel.b * level * (double)(light.channel.b / 255.0));
+	final.channel.r = (uint8_t)ft_min(color_calc.x, 255);
+	final.channel.g = (uint8_t)ft_min(color_calc.y, 255);
+	final.channel.b = (uint8_t)ft_min(color_calc.z, 255);
+	return (final);
+}
+
+static t_3d	dir_to_light(t_3d light_origin, t_3d origin, double *t)
+{
+	t_3d	light_dir;
+
+	light_dir = subtract_vectors(light_origin, origin);
+	*t = vector_magnitude(light_dir);
+	light_dir = normalize_vector(light_dir);
+	return (light_dir);
+}
+
+uint32_t	light_up(t_list *object_list, t_color obj_color, t_ray to_light, t_3d normal)
+{
+	t_list		*object_list_start;
+	t_object	*object;
+	t_color		color;
+	double		t;
+	double		level;
+
+	ft_bzero(&color, sizeof(t_color));
+	object_list_start = object_list;
+	while (object_list != NULL)
+	{
+		object = (t_object *)object_list->content;
+		if (object->type == LIGHT)
+		{
+			to_light.forward = dir_to_light(object->origin, to_light.origin, &t);
+			if (t <= intersect_loop(&to_light, object_list_start, NULL).x)
+			{
+				level = get_light_level(t, object->lumen, normal, to_light.forward);
+				color = calc_light(color, object->color, obj_color, level);
+			}
+		}
+		object_list = object_list->next;
+	}
+	return (color.combined);
+}
