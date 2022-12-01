2608096129ae2fca6850be935c2b5c65068816d1
Conu101 <constance.trouve@gmail.com>
2022-12-01 16:25:38 +0200

    work on plane checkerboard: horizontal plane working

--- sources/mapping/checkerboard.c
+++ sources/mapping/checkerboard.c
@@ -6,7 +6,7 @@
 /*   By: ctrouve <ctrouve@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 15:28:49 by ctrouve           #+#    #+#             */
-/*   Updated: 2022/11/30 16:33:26 by ctrouve          ###   ########.fr       */
+/*   Updated: 2022/12/01 16:14:32 by ctrouve          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,19 +51,38 @@ t_2d	spherical_map(t_object sphere, t_3d p)
 	return (pos);
 }
 
-static t_2d	planar_map(t_3d p)
+/*
+** First we translate the coord into new_coord in a horizontal plan (0, 1, 0)
+** then in 2D into this plan by negating the y coordinate.
+*/
+static t_2d	planar_map(t_object hit_object, t_3d hit_point)
 {
-	t_2d	pos;
+	t_2d	coord_2d;
+	t_3d	abc_param;
+	double	d;
+	double	k;
 	t_3d	new_coord;
 
-	
-
-	pos.x = floor(p.x);
-	pos.y = floor(p.z);
-	return (pos);
+	abc_param.x = hit_object.axis.x;
+	abc_param.y = hit_object.axis.y;
+	abc_param.z = hit_object.axis.z;
+	// abc_param.x = 0;
+	// abc_param.y = 1;
+	// abc_param.z = 0;
+	d = hit_object.axis.x * hit_object.origin.x + hit_object.axis.y * hit_object.origin.y + hit_object.axis.z * hit_object.origin.z;
+//	d = hit_object.origin.y;
+	k = (d - (abc_param.x * hit_point.x) - (abc_param.y * hit_point.y) - (abc_param.z * hit_point.z)) /\
+	 (abc_param.x * abc_param.x + abc_param.y * abc_param.y + abc_param.z * abc_param.z);
+	new_coord.x = hit_point.x + k * abc_param.x;
+	new_coord.y = hit_point.y + k * abc_param.y;
+	new_coord.z = hit_point.z + k * abc_param.z;
+	coord_2d.x = new_coord.x;
+	coord_2d.y = new_coord.z;
+	return (coord_2d);
 }
 
-/* If you want your checkers to look "square" on the sphere, be sure and set 
+/*
+** If you want your checkers to look "square" on the sphere, be sure and set 
 ** the width to twice the height. This is because of how the spherical map is 
 ** implemented. While both u and v go from 0 to 1, v maps 1 to π, but u maps 
 ** 1 to 2π. -> width = 2*height in init_bw_checker
@@ -77,17 +96,19 @@ t_color	define_checker_color(t_object *hit_object, t_3d hit_point)
 	t_color		color_b;
 	double		height;
 
+//	height = hit_object->radius / 2;
 	color_a.combined = hit_object->color.combined;
 	color_b.combined = 0xFFFFFF;
-	height = hit_object->radius / 2;
 	if (hit_object->type == SPHERE)
 	{
+		height = hit_object->radius / 2;
 		uv_pos = spherical_map(*hit_object, hit_point);
 		checker = init_checker(2 * height, height, color_a, color_b);
 	}
 	else
 	{
-		uv_pos = planar_map(hit_point);
+		height = 10;
+		uv_pos = planar_map(*hit_object, hit_point);
 		checker = init_checker(height, height, color_a, color_b);
 	}
 	color_out = uv_pattern_at(checker, uv_pos.x, uv_pos.y);
