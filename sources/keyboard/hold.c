fa4db56e8fc0ef2174700c8d93f3d13f76ff4e80
PietarTheWise <pietari.noutere@gmail.com>
2022-12-15 15:11:25 +0200

    fixed norm for keyboard files

--- sources/keyboard/hold.c
+++ sources/keyboard/hold.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   hold.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dmalesev <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: pnoutere <pnoutere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 15:53:27 by dmalesev          #+#    #+#             */
-/*   Updated: 2022/11/10 16:08:41 by dmalesev         ###   ########.fr       */
+/*   Updated: 2022/12/15 15:10:15 by pnoutere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,23 @@ int	move_camera(t_env *env)
 	if ((env->keymap & 0xFFFFFFFF) == 0)
 		return (0);
 	if ((env->keymap & KEY_A) == KEY_A)
-		camera->ray.origin = add_vectors(camera->ray.origin, scale_vector(camera->right, -1));
+		camera->ray.origin = add_vectors(camera->ray.origin,
+				scale_vector(camera->right, -1));
 	if ((env->keymap & KEY_W) == KEY_W)
-		camera->ray.origin = add_vectors(camera->ray.origin, camera->ray.forward);
+		camera->ray.origin = add_vectors(camera->ray.origin,
+				camera->ray.forward);
 	if ((env->keymap & KEY_D) == KEY_D)
-		camera->ray.origin = add_vectors(camera->ray.origin, camera->right);
+		camera->ray.origin = add_vectors(camera->ray.origin,
+				camera->right);
 	if ((env->keymap & KEY_S) == KEY_S)
-		camera->ray.origin = add_vectors(camera->ray.origin, scale_vector(camera->ray.forward, -1));
+		camera->ray.origin = add_vectors(camera->ray.origin,
+				scale_vector(camera->ray.forward, -1));
 	if ((env->keymap & KEY_SPACE) == KEY_SPACE)
-		camera->ray.origin = add_vectors(camera->ray.origin, (t_3d){0.0f, 1.0f, 0.0f});
+		camera->ray.origin = add_vectors(camera->ray.origin,
+				(t_3d){0.0f, 1.0f, 0.0f});
 	if ((env->keymap & KEY_LSHIFT) == KEY_LSHIFT)
-		camera->ray.origin = add_vectors(camera->ray.origin, (t_3d){0.0f, -1.0f, 0.0f});
+		camera->ray.origin = add_vectors(camera->ray.origin,
+				(t_3d){0.0f, -1.0f, 0.0f});
 	return (1);
 }
 
